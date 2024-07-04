91b6e7482b7f5e1c6884f51d5419941ddffb6e6e
EslamKhalaf9 <eslamkhalaf191@gmail.com>
2024-07-04 07:09:23 +0300

    made navbar title to be a linke to the home page

--- src/components/layout/main-navbar.tsx
+++ src/components/layout/main-navbar.tsx
@@ -5,7 +5,9 @@ export default function MainNavbar() {
   return (
     <nav className="border-b-4 border-primary bg-background fixed w-full">
       <div className="container h-[10vh] flex items-center md:justify-between justify-center justify-items-center">
-        <h1 className="lg:text-3xl uppercase text-md md:block hidden">Eslam Khalaf</h1>
+        <Link href="/">
+          <h1 className="text-3xl uppercase text-foreground">Eslam Khalaf</h1>
+        </Link>
         <ul className="flex md:gap-6 gap-3">
           <li>
             <Link href="#landing" className=" hover:text-foreground transition-colors text-xl">Home</Link>
